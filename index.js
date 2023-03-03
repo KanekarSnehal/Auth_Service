9775d877226277b89add3adafd1530c8d8f5e907
KanekarSnehal <snehalkanekar43@gmail.com>
2023-03-03 13:55:28 +0530

    initial repository setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const { PORT } = require("./config/serverConfig")
+
+const prepareAndStartServer = () => {
+    app.listen(PORT, () => {
+        console.log(`Server Started at ${PORT}`);
+    })
+}
+
+prepareAndStartServer();

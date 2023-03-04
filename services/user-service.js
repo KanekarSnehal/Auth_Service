b867c665759eba90579906d29f2a52f05bfc24fa
KanekarSnehal <snehalkanekar43@gmail.com>
2023-03-04 11:42:25 +0530

    feat: aded token create and verify logic

--- services/user-service.js
+++ services/user-service.js
@@ -1,5 +1,6 @@
+const jwt = require('jsonwebtoken');
 const UserRepository = require("../repository/user-repository");
-
+const { JWT_KEY } = require('../config/serverConfig');
 class UserService {
     constructor() {
         this.userRepository = new UserRepository();
@@ -11,6 +12,26 @@ class UserService {
             return user;
         }
         catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' });
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
             console.log("Something went wrong in repository layer");
             throw error;
         }

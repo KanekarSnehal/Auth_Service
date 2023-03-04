c82fae3530baf81c9ebf1332e10e3637156ed1ae
KanekarSnehal <snehalkanekar43@gmail.com>
2023-03-04 11:47:04 +0530

    feat: compare passwords

--- services/user-service.js
+++ services/user-service.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const UserRepository = require("../repository/user-repository");
 const { JWT_KEY } = require('../config/serverConfig');
+
 class UserService {
     constructor() {
         this.userRepository = new UserRepository();
@@ -31,6 +33,15 @@ class UserService {
         try {
             const response = jwt.verify(token, JWT_KEY);
             return response;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
         } catch (error) {
             console.log("Something went wrong in repository layer");
             throw error;
